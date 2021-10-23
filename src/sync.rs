e36cec8ef8aaec206449cd1184ef2be4565bd5fe
MinusKelvin <mark.carlson@minuskelvin.net>
2021-10-23 17:46:30 +1100

    log overall nps

--- src/sync.rs
+++ src/sync.rs
@@ -19,6 +19,8 @@ impl BotSyncronizer {
                 stats: Default::default(),
                 last_advance: Instant::now(),
                 node_limit: u64::MAX,
+                start: Instant::now(),
+                nodes_since_start: 0,
             }),
             blocker: Condvar::new(),
             bot: RwLock::new(None),
@@ -28,6 +30,8 @@ impl BotSyncronizer {
     pub fn start(&self, initial_state: Bot) {
         let mut state = self.state.lock();
         state.stats = Default::default();
+        state.nodes_since_start = 0;
+        state.start = Instant::now();
         *self.bot.write() = Some(initial_state);
         self.blocker.notify_all();
     }
@@ -49,8 +53,11 @@ impl BotSyncronizer {
                     ),
                     depth: None,
                     extra: Some(format!(
-                        "{:.1}% of selections expanded",
-                        state.stats.expansions as f64 / state.stats.selections as f64 * 100.0
+                        "{:.1}% of selections expanded, overall speed: {:.1} Mnps",
+                        state.stats.expansions as f64 / state.stats.selections as f64 * 100.0,
+                        state.nodes_since_start as f64
+                            / state.start.elapsed().as_secs_f64()
+                            / 1_000_000.0
                     )),
                 },
             )
@@ -99,6 +106,7 @@ impl BotSyncronizer {
 
             state = self.state.lock();
             state.stats.accumulate(new_stats);
+            state.nodes_since_start += new_stats.nodes;
         }
     }
 }
@@ -108,4 +116,6 @@ struct State {
     stats: Statistics,
     last_advance: Instant,
     node_limit: u64,
+    start: Instant,
+    nodes_since_start: u64,
 }
